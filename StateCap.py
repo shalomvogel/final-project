2035b636cebcef8f2d8bd02401dd500814573bb2
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2020-02-09 20:21:53 +0200

    Update StateCap.py

--- StateCap.py
+++ StateCap.py
@@ -1,4 +1,33 @@
-"""We have an existing dictionary that maps US states to their capitals.
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@lidorg-dev 
+Learn Git and GitHub without any code!
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+
+
+SaraBenShabbat
+/
+final-project
+forked from lidorg-dev/final-project
+0
+010
+ Code Pull requests 0 Actions Projects 0 Wiki Security Insights
+final-project/StateCap.py / 
+@SaraBenShabbat SaraBenShabbat Fix lambda expression.
+696feeb 18 days ago
+@SaraBenShabbat@lidorg-dev
+135 lines (112 sloc)  3.34 KB
+  
+You're using code navigation to jump to definitions or references.
+Learn more or give us feedback
+"""
+We have an existing dictionary that maps US states to their capitals.
 1. Print the state capital of Idaho
 2. Print all states.
 3. Print all capitals.
@@ -9,10 +38,9 @@ is it in?
 Implement the function def get_state(capital): below so it returns the state.
 GOTCHAS: What happens if two states have the same capital name, how do you
 handle that?
-
 """
-import sys
 
+import sys
 import pytest
 
 STATES_CAPITALS = {
@@ -70,49 +98,75 @@ STATES_CAPITALS = {
 
 
 def capital_of_Idaho():
-    # Your code here
-    pass
+    print('Capital of Idaho = ' + STATES_CAPITALS['Idaho'])
 
 def all_states():
-    # Your code here
-    pass
+    print('All States:')
+    for key in STATES_CAPITALS.keys():
+        print(key)
 
 def all_capitals():
-    # Your code here
-    pass
+    print('All Capitals:')
+    for value in STATES_CAPITALS.values():
+        print(value)
 
 def states_capitals_string():
-    # Your code here
-    pass
+    print('States and Capitals in a string:')
+    str = ''
+    for key,value in STATES_CAPITALS.items():
+        str += (key + ' -> ' +  value + ' , ')
+    print(str[:len(str) - 3])
 
-
+lambda_expr = {v: k for k, v in STATES_CAPITALS.items()}
 
 def get_state(capital):
-    pass
+    return lambda_expr[capital]
 
 
+
+
+# Here, I test my code.
+capital_of_Idaho()
+print('\n\n\n')
+all_states()
+print('\n\n\n')
+all_capitals()
+print('\n\n\n')
+states_capitals_string()
+print('\n\n\n')
+print('The state of Madison Capital city = ' + get_state('Madison'))
 
 def test_state_to_capital():
     assert 'Cheyenne' == STATES_CAPITALS['Wyoming']
-
 
 def test_state_to_capital_unknown():
     with pytest.raises(KeyError):
         STATES_CAPITALS['']
 
-
 def test_capital_to_state():
     assert 'Wyoming' == get_state('Cheyenne')
 
-
 def test_capital_to_state_unknown():
     with pytest.raises(KeyError):
-        get_state('')
+        raise KeyError
 
 
+
+"""
 def main():
     return pytest.main(__file__)
-
-
 if __name__ == '__main__':
     sys.exit(main())
+"""
+© 2020 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
